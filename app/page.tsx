fe421c4a2181dc42d2d05394c3adcba403f10842
SmeyerGit <nilsschmeierprio@gmail.com>
2024-09-23 01:04:44 +0200

    table

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import {  Table,  TableHeader,  TableBody,  TableColumn,  TableRow,  TableCell} from "@nextui-org/table";
 
 export default function Home() {
   return (
@@ -17,6 +18,36 @@ export default function Home() {
           <li>Was geht ab?</li>
           <li>Willst du mir helfen?</li>
         </ol>
+        <Table aria-label="Example static collection table">
+          <TableHeader>
+            <TableColumn>NAME</TableColumn>
+            <TableColumn>ROLE</TableColumn>
+            <TableColumn>STATUS</TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow key="1">
+              <TableCell>Tony Reichert</TableCell>
+              <TableCell>CEO</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="2">
+              <TableCell>Zoey Lang</TableCell>
+              <TableCell>Technical Lead</TableCell>
+              <TableCell>Paused</TableCell>
+            </TableRow>
+            <TableRow key="3">
+              <TableCell>Jane Fisher</TableCell>
+              <TableCell>Senior Developer</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="4">
+              <TableCell>William Howard</TableCell>
+              <TableCell>Community Manager</TableCell>
+              <TableCell>Vacation</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
