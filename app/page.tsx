c22200850295bb8ef721701a3fc9d17381d9b705
SmeyerGit <nilsschmeierprio@gmail.com>
2024-09-26 01:22:06 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 "use client";
 import Image from "next/image";
-import { Fade } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
 
 
-const fadeImages = [
-  "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-  "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-  "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-];
+
+
 
 export default function Home() {
   return (
@@ -27,22 +22,6 @@ export default function Home() {
           <li>Was geht ab?</li>
           <li>Willst du mir helfen?</li>
         </ol>
-
-        <div className="slide-container">
-      <Fade>
-        <div className="each-fade">
-          <img src={fadeImages[0]} />
-        </div>
-        <div className="each-fade">
-          <img src={fadeImages[1]} />
-        </div>
-        <div className="each-fade">
-          <img src={fadeImages[2]} />
-        </div>
-      </Fade>
-    </div>
-
-        
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
